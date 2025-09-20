1b39f3150413f5ec33acbb099a0803bab553798e
adamnielsen32 <nie15046@byui.edu>
2025-09-20 15:59:19 -0600

    week1 nps

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const heroImg = document.querySelector(".hero-banner img");
+heroImg.src = parkData.images[0].url;
+heroImg.alt = parkData.images[0].altText;
+
+const heroName = document.querySelector(".hero-banner__title");
+const heroSubtitle = document.querySelector(".hero-banner__subtitle");
+
+heroName.textContent = parkData.fullName;
+heroSubtitle.innerHTML = `
+  <span>${parkData.designation}</span>
+  <span>${parkData.states}</span>
+`;
