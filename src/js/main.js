b9bc1e3a10dff4a88c79222359296a2a78a615a0
adamnielsen32 <nie15046@byui.edu>
2025-09-26 11:25:12 -0600

    week2 changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,23 @@
-import { getParkData } from "./parkService.mjs";
+
+import { getParkData, parkInfoLinks } from "./parkService.mjs";
+import setHeaderFooter from "./setHeaderFooter.mjs";
+import { mediaCardTemplate } from "./templates.mjs";
+
 
 const parkData = getParkData();
 
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+function setParkIntro(data) {
+  const introEl = document.querySelector(".intro");
+  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
+  <p>${parkData.description}</p>`;
+}
 
-document.title = parkData.fullName;
+function setParkInfoLinks(data) {
+  const infoEl = document.querySelector(".info");
+  const html = data.map(mediaCardTemplate);
+  infoEl.insertAdjacentHTML("afterbegin", html.join(""));
+}
 
-const heroImg = document.querySelector(".hero-banner img");
-heroImg.src = parkData.images[0].url;
-heroImg.alt = parkData.images[0].altText;
-
-const heroName = document.querySelector(".hero-banner__title");
-const heroSubtitle = document.querySelector(".hero-banner__subtitle");
-
-heroName.textContent = parkData.fullName;
-heroSubtitle.innerHTML = `
-  <span>${parkData.designation}</span>
-  <span>${parkData.states}</span>
-`;
+setHeaderFooter(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
---
+++ src/js/setHeaderFooter.mjs
@@ -0,0 +1,30 @@
+import { parkInfoTemplate, footerTemplate } from "./templates.mjs";
+
+function setHeaderInfo(data) {
+  const disclaimer = document.querySelector(".disclaimer > a");
+  disclaimer.href = data.url;
+  disclaimer.innerHTML = data.fullName;
+
+  document.title = data.fullName;
+
+  const heroImg = document.querySelector(".hero-banner img");
+  heroImg.src = data.images[0].url;
+  heroImg.alt = data.images[0].altText;
+
+  document.querySelector(".hero-banner__title").textContent = data.fullName;
+  document.querySelector(".hero-banner__subtitle").innerHTML = `
+    <span>${data.designation}</span>
+    <span>${data.states}</span>
+  `;
+}
+
+function setFooter(data) {
+  const footerEl = document.querySelector("#park-footer");
+  footerEl.insertAdjacentHTML("afterbegin", footerTemplate(data));
+}
+
+export default function setHeaderFooter(parkData) {
+  setHeaderInfo(parkData);
+  setFooter(parkData);
+}
+
